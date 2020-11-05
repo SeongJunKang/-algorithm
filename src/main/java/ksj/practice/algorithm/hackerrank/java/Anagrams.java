9fab710b3537cbea938d8b7cf1614b7029c9f96b
kang seongjun <tjdwns8574@gmail.com>
2020-11-05 11:22:19 +0900

    update : add hackerrank java practice
    java-anagrams

---
+++ src/main/java/ksj/practice/algorithm/hackerrank/java/Anagrams.java
@@ -0,0 +1,43 @@
+package ksj.practice.algorithm.hackerrank.java;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Anagrams {
+
+    static boolean isAnagram(String a, String b) {
+        List<Character> aList = new ArrayList<>(
+                a.toLowerCase().chars()
+                .mapToObj(c -> (char)c)
+                .collect(Collectors.toList())
+        );
+        List<Character> bList = new ArrayList<>(
+                b.toLowerCase().chars()
+                        .mapToObj(c -> (char)c)
+                        .collect(Collectors.toList())
+        );
+
+        if(aList.size() != bList.size()) return false;
+
+        Collections.sort(aList);
+        Collections.sort(bList);
+
+        for(int i = 0 ; i < aList.size(); i++) {
+            if(!aList.get(i).equals(bList.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
