fb9b2448fe55583cf4f4912c70de95b55571f4f4
kang seongjun <tjdwns8574@gmail.com>
2020-11-09 10:58:13 +0900

    update : add hackerrank java practice

--- src/main/java/ksj/practice/algorithm/hackerrank/practice/java/JavaStringTokens.java
+++ src/main/java/ksj/practice/algorithm/hackerrank/practice/java/JavaStringTokens.java
@@ -45,13 +45,15 @@ public class JavaStringTokens {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
-        s = s.replaceAll("[!,?._'@]"," ")
-                .replaceAll("  ", " ");
-
-        String[] split = s.split(" ");
-        System.out.println(""+split.length);
-        for (String word : split) {
-            System.out.println(word);
+        s = s.trim();
+        if (s.length()==0 || s.length()>400000) {
+            System.out.println(0);
+        } else {
+            String[] split = s.split("[ !,?.\\_'@]+");
+            System.out.println(split.length);
+            for (String word : split) {
+                System.out.println(word);
+            }
         }
         scan.close();
     }
